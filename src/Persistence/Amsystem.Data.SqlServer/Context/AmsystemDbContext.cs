c775326ca76ca7731694511747fb04339f315dfe
Yurian Jimenez Rubalcaba <yjimenez@uci.cu>
2017-11-27 12:02:11 -0500

    Add database basic context for performing operations using the ORM Entity Framework v6.1.3

---
+++ src/Persistence/Amsystem.Data.SqlServer/Context/AmsystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amsystem.Data;
+using Amsystem.Data.Entities;
+
+namespace Amsystem.Data.SqlServer.Context
+{
+    public class AmsystemDbContext : DbContext
+    {
+        public AmsystemDbContext() : base("AmsystemDbConnection")
+        {
+            
+        }
+
+        public DbSet<Agreement> Agreements { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
