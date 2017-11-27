dfb0d736d59822862993f2c9f5a69f9994354182
Yurian Jimenez Rubalcaba <yjimenez@uci.cu>
2017-11-27 11:07:36 -0500

    Add a generic repository interface for performing operations over entities

---
+++ src/Persistence/Amsystem.Data/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amsystem.Data.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null);
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+        void Update(TEntity entity);
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
