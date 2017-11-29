d22ad5bb6dcf453894ab9d0f33ffeed497629643
Yurian Jimenez Rubalcaba <yurian89@gmx.es>
2017-11-29 00:20:44 -0800

    Create basic Autofac settings

---
+++ src/Amsystem/App_Start/AutofacConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Amsystem.Data.SqlServer.Context;
+using Amsystem.Persistense;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace Amsystem
+{
+    public class AutofacConfig
+    {
+        public static IContainer ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType(typeof(AmsystemDbContext)).InstancePerRequest();
+            builder.RegisterType(typeof(UnitOfWork)).As(typeof(IUnitOfWork)).InstancePerRequest();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            var container = builder.Build();
+            return container;
+        }
+    }
+}
