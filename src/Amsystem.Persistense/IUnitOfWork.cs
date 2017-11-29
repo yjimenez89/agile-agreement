c6fd6615ac86e09a1b087681fdbc55e35393ead8
Yurian Jimenez Rubalcaba <yurian89@gmx.es>
2017-11-28 23:28:37 -0800

    Create a basic generic version of the Work Unit pattern to avoid duplicate codes and queries.

---
+++ src/Amsystem.Persistense/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amsystem.Data.Repositories;
+
+namespace Amsystem.Persistense
+{
+    public interface IUnitOfWork
+    {
+        IRepository<TEntity> Repository<TEntity>() where TEntity : class;
+        void SaveChanges();
+    }
+}
--- src/Amsystem.Persistense/UnitOfWork.cs
+++ src/Amsystem.Persistense/UnitOfWork.cs
@@ -4,25 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Amsystem.Data.Entities;
+using Amsystem.Data.Repositories;
 using Amsystem.Data.SqlServer.Context;
 using Amsystem.Persistense.Repositories;
 
 namespace Amsystem.Persistense
 {
-    public class UnitOfWork
+    public class UnitOfWork : IDisposable, IUnitOfWork
     {
         private readonly AmsystemDbContext _dbContext;
-        private Repository<Agreement> _agreementRepository;
-        private Repository<Status> _statusRepository;
-        private Repository<User> _userRepository;
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
 
         public UnitOfWork(AmsystemDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        public Repository<Agreement> AgreementRepository => _agreementRepository ?? (_agreementRepository = new Repository<Agreement>(_dbContext));
-        public Repository<Status> StatusRepository => _statusRepository ?? (_statusRepository = new Repository<Status>(_dbContext));
-        public Repository<User> UserRepository => _userRepository ?? (_userRepository = new Repository<User>(_dbContext));
+        public IRepository<TEntity> Repository<TEntity>() where TEntity : class
+        {
+            if (_repositories.Keys.Contains(typeof(TEntity)))
+            {
+                return _repositories[typeof(TEntity)] as IRepository<TEntity>;
+            }
+
+            IRepository<TEntity> repo = new Repository<TEntity>(_dbContext);
+            _repositories.Add(typeof(TEntity), repo);
+            return repo;
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _dbContext?.Dispose();
+        }
     }
 }
